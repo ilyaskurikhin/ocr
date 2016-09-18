405c4b682c4f27617e3057e6de8c63164aad2246
eson-schulz <ethan@eschulz.com>
2016-09-17 20:12:55 -0500

    Camera now takes pictures - doesn't save yet

--- src/main/java/com/ocrsplitter/ocrsplitter/HomeActivity.java
+++ src/main/java/com/ocrsplitter/ocrsplitter/HomeActivity.java
@@ -3,6 +3,7 @@ package com.ocrsplitter.ocrsplitter;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -18,9 +19,29 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class HomeActivity extends AppCompatActivity {
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    Button cameraButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
+
+        cameraButton = (Button) findViewById(R.id.cameraButton);
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 }
---
+++ src/main/java/com/ocrsplitter/ocrsplitter/ReceiptItem.java
@@ -0,0 +1,15 @@
+package com.ocrsplitter.ocrsplitter;
+
+/**
+ * Created by eson on 9/17/16.
+ */
+public class ReceiptItem {
+
+    private String name;
+    private double price;
+
+    public ReceiptItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+}
